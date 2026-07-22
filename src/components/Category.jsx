82d898b2f043796d65fcf2a11ee3d2da64bb56d2
kazikomol <kazikomol2@gmail.com>
2026-07-22 23:47:43 +0600

    last deploy

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,17 +1,25 @@
 import { Button } from '@heroui/react';
+import Link from 'next/link';
 import React from 'react';
 
 const Category = async () => {
-    const res = await fetch('https://tiles-gallery-a8-three.vercel.app/category.json')
-    const categories = await res.json()  
-      return (
-        <div className='mb-5 space-x-3'>
-            {
-                categories.map(category => <Button variant='outline' size='sm' key={category.id}>{category.name}</Button>)
-            }
-            
-        </div>
-    );
+  const res = await fetch('https://tiles-gallery-a8-three.vercel.app/category.json');
+  const categories = await res.json();
+
+  return (
+    <div className='mb-5 space-x-3'>
+      {categories.map((category) => (
+        <Link
+          key={category.id}
+          href={`?category=${category.name.toLowerCase()}`}
+        >
+          <Button variant='bordered' size='sm'>
+            {category.name}
+          </Button>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Category;
