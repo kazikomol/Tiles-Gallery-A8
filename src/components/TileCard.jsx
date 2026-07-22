61e8ddba5f6b516db55e40cabf070fbc857214e6
kazikomol <kazikomol2@gmail.com>
2026-07-22 14:12:01 +0600

    Image Updated

--- src/components/TileCard.jsx
+++ src/components/TileCard.jsx
@@ -1,15 +1,35 @@
 "use client"
-import { Card } from '@heroui/react';
+import { Button, Card, Chip } from '@heroui/react';
 import Image from 'next/image';
 import React from 'react';
+import { BiDollar } from 'react-icons/bi';
 
 const TileCard = ({tiles}) => {
     console.log(tiles)
     return (
         
 
-            <Card >
-                <Image src={tiles.image} alt='tiles' width={100} height={100}  />
+            <Card className='items-center border' >
+                <div className='relative w-full aspect-square'>
+                     <Image 
+                src={tiles.image} 
+                alt='tiles' 
+                
+            fill 
+                className='object-cover rounded-xl'
+                /> 
+                <Chip size='sm' className='absolute m-1'>{tiles.category}</Chip>
+                
+                </div>
+                <div>
+                    <h2 className='font-medium'>{tiles.title}</h2>
+                </div>
+                <div className='flex gap-between '>
+                  <BiDollar className='size-6' /> <p className='text-xl'>{tiles.price}{tiles.currency}</p>
+                </div>
+               
+                    <Button className={'w-full'} variant="secondary">Details</Button>
+                
             </Card>
             
         
--- src/components/TopFour.jsx
+++ src/components/TopFour.jsx
@@ -5,32 +5,27 @@ import Banner from './Banner';
 const TopFour = async () => {
 const res = await fetch('https://tiles-gallery-a8-three.vercel.app/data.json')
 const data = await res.json()
-
-
 const Top= data.slice(0,4);
 console.log(Top);
 
     return (
-       
-
-
         <div>
-
             <h2 className='text-4xl font-bold text-center mt-10'>Top Living Example of architect</h2>
-
             <div>
                 {
-                    Top.map(tiles => <div  key={tiles.id} className=' grid-cols-2 gap-4' >
-                        <TileCard tiles={tiles} />
+                    <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-4 p-6 ">
+                    {Top.map((tiles) =>  (
+                        <TileCard key={tiles.id} tiles={tiles} />)
+
+    
+
                         
-                        
+                        )}
                         </div>
-                        )
                 }
             </div>
-            
+
         </div>
     );
 };
-
 export default TopFour;
